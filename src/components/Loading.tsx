48c8b9cdae3f686844a2b1c4d8f5eb06c5a19a58
Guru901 <avinashkaur842@gmail.com>
2024-12-17 06:00:16 +0000

    added loading comp

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from "lucide-react";
 
 export function Loader() {
   return (
-    <div className="w-full h-[] flex items-center justify-center">
-      <Loader2 className="animate-spin-css" />
+    <div className="w-full h-[40vh] flex items-center justify-center">
+      <Loader2 className="animate-spin-css text-gray-400" />
     </div>
   );
 }
