6a402d7c71cd8ca47e7616229904a3cc3bc8ee5f
Gurvinder Singh <avinashkaur842@gmail.com>
2024-12-28 00:52:24 +0530

    i guess now it works

--- src/app/api/get-productivity-data/route.ts
+++ src/app/api/get-productivity-data/route.ts
@@ -1,62 +1,65 @@
-import { connectToDB } from "@/lib/db-connect";
-import { Form } from "@/models/form-model";
-import { NextRequest, NextResponse } from "next/server";
+import {connectToDB} from "@/lib/db-connect";
+import {Form} from "@/models/form-model";
+import {NextRequest, NextResponse} from "next/server";
 
 export async function GET(request: NextRequest) {
-  try {
-    await connectToDB();
+    try {
+        await connectToDB();
 
-    const id = request.nextUrl.searchParams.get("id");
-    const productivityData: {
-      date: string;
-      productivity: string;
-    }[] = []
+        const id = request.nextUrl.searchParams.get("id");
+        const productivityData: {
+            date: string;
+            productivity: string;
+        }[] = []
 
-    if (!id) {
-      return NextResponse.json(
-        { message: "User not found", success: false },
-        {
-          status: 404,
+        if (!id) {
+            return NextResponse.json(
+                {message: "User not found", success: false},
+                {
+                    status: 404,
+                }
+            );
         }
-      );
-    }
 
-    const forms = await Form.find({
-      createdBy: id,
-    }).select("productivity createdAt").sort({
-      createdAt: -1
-    }).limit(30);
+        const forms = await Form.find({
+            createdBy: id,
+        }).select("productivity createdAt").sort({
+            createdAt: -1
+        }).limit(30);
 
 
-    if (!forms.length) {
-      return NextResponse.json(
-        { message: "User not found", success: false },
-        {
-          status: 404,
+        if (!forms.length) {
+            return NextResponse.json(
+                {message: "User not found", success: false},
+                {
+                    status: 404,
+                }
+            );
         }
-      );
+
+        forms.forEach(form => {
+            let date = form.createdAt.toLocaleDateString();
+            date = date.split('/')
+            const formattedDate = `${date[1]}/${date[0]}/${date[2]}`
+            productivityData.push({
+                productivity: form.productivity,
+                date: formattedDate,
+            })
+        })
+
+        return NextResponse.json(
+            {success: true, data: {productivityData}},
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            {message: "Error", success: false},
+            {
+                status: 500,
+            }
+        );
     }
-
-    forms.forEach(form => {
-      productivityData.push({
-        productivity: form.productivity,
-        date: form.createdAt.toLocaleDateString(),
-      })
-    })
-
-    return NextResponse.json(
-      { success: true, data: { productivityData } },
-      {
-        status: 200,
-      }
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Error", success: false },
-      {
-        status: 500,
-      }
-    );
-  }
 }
